65cf641fd4cca57816594f6f3a4b71f6a2b724ea
Wesley  Oliveira <wmoraisoliveira@gmail.com>
2018-04-26 14:13:56 -0300

    Ajuste nos botoes

--- src/components/todolist.jsx
+++ src/components/todolist.jsx
@@ -9,7 +9,7 @@ export default props =>{
       <div className="todoitem" key={todo._id}>
         <p className={todo.done ? 'markAsDone': ''}>{todo.description}</p>
         <div className="buttons-todo">
-        <button className='sucess' hide={todo.done} onClick={() => props.handleMarkAsDone(todo)}>✓</button>
+        <button className='sucess' title="teste" hide={todo.done} onClick={() => props.handleMarkAsDone(todo)}>✓</button>
         <button className='pending' hide={!todo.done} onClick={() => props.handleMarkAsPending(todo)}>⟲</button>
         <button className='delete' hide={!todo.done} onClick={() => props.handleRemove(todo)}>X</button>
         </div>
@@ -20,7 +20,5 @@ export default props =>{
    <div className="todolist">
     {renderRows()}
    </div>
-
-
  )
 }
